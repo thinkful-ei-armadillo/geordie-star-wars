8f829fbfd61087520bae049ca88e4bb98cfaf989
Geordie Connell <geordie.connell@gmail.com>
2019-02-15 14:55:44 -0500

    Search component complete

--- star-wars/src/App.js
+++ star-wars/src/App.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
+import Search from "./Search";
 
 class App extends Component {
   state = {
     searchTerm: null,
     characterList: []
   }
+
+  userInput = (event) => {
+    event.preventDefault();
+    const searchValue = event.currentTarget.search.value;
+    this.setState({
+      searchTerm: searchValue
+    })
+    fetch(`https://swapi.co/api/people/?search=${searchValue}`)
+      .then(resJson => resJson.json())
+      .then(res => res.results.map(char => {
+        return {
+          name: char.name
+        }
+      }))
+      .then(data => this.setState({
+        characterList: data
+      }));
+  };
+
   render() {
     return (
       <div className="App">
-      <h1>Star Wars</h1>
+        <Search userInput={this.userInput} />
       </div>
     );
   }
---
+++ star-wars/src/Search.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Search extends Component {
+
+    render() {
+        return (
+            <form onSubmit={ this.props.userInput }>
+                <label forhtml="search">Character Search:</label>
+                <input name="search" type="search" id="search" placeholder="EX: Han Solo"/>
+                <input name="search-submit" type="submit" value="GO!"/>
+            </form>
+        )
+    }
+}
+
+export default Search;
